5d2c6595296624a5a35da92498a0dd3d3d717f1e
kyranet <kyradiscord@gmail.com>
2018-11-18 16:10:26 +0100

    misc: Limit inspect to 0

--- src/ipcMonitors/eval.ts
+++ src/ipcMonitors/eval.ts
@@ -6,9 +6,9 @@ export default class extends IPCMonitor {
 	public async run(payload: string): Promise<any> {
 		if (!payload) throw 'MISSING_PAYLOAD';
 		try {
-			return inspect(await eval(payload));
+			return inspect(await eval(payload), { depth: 0, showProxy: true });
 		} catch (error) {
-			return inspect(error);
+			return inspect(error, { depth: 0, showProxy: true });
 		}
 	}
 
