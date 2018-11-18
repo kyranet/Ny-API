d8e72c05facc760460ca956090d950e00368c712
kyranet <kyradiscord@gmail.com>
2018-11-18 16:36:21 +0100

    misc: Change IPC to TCP

--- src/NyAPI.ts
+++ src/NyAPI.ts
@@ -1,10 +1,9 @@
-import { platform } from 'os';
 import { inspect } from 'util';
 import { APIClient } from './lib/APIClient';
 inspect.defaultOptions.depth = 1;
 
 const client = new APIClient({ dashboardHooks: { port: 8282 } });
-client.ipc.serve(platform() === 'win32' ? '//./pipe/tmp/NyAPI.sock' : '/tmp/NyAPI.sock')
+client.ipc.serve(9997)
 	.catch((error) => { client.console.error(error); });
 client.start()
 	.catch((error) => { client.console.error(error); });
