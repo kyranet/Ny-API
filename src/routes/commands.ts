e1981b758d6924fa046e666437ba8a2d03281bb3
kyranet <kyradiscord@gmail.com>
2019-06-01 01:33:18 +0200

    feat: Added commands and statistics endpoints

---
+++ src/routes/commands.ts
@@ -0,0 +1,25 @@
+import { ServerResponse } from 'http';
+import { inspect } from 'util';
+import { APIClient, Sockets } from '../lib/APIClient';
+import { DashboardClient, KlasaIncomingMessage, Route, RouteStore } from '../lib/third_party/klasa-dashboard-hooks';
+
+export default class extends Route {
+
+	public client: APIClient;
+
+	public constructor(client: DashboardClient, store: RouteStore, file: string[], directory: string) {
+		super(client, store, file, directory, { route: '/invite' });
+	}
+
+	public async get(_: KlasaIncomingMessage, response: ServerResponse): Promise<void> {
+		try {
+			const commands = await this.client.ipcRequest<{ response: string }>(Sockets.Skyra, ['commands']);
+			response.writeHead(200);
+			response.end(JSON.stringify({ success: true, data: commands }));
+		} catch (error) {
+			response.writeHead(500);
+			response.end(JSON.stringify({ success: false, data: inspect(error) }));
+		}
+	}
+
+}
