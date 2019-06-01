97f686d7af699e83241f945f5b48c88873d61fff
kyranet <kyradiscord@gmail.com>
2019-06-01 17:25:10 +0200

    fix: Trying to resolve this CORB annoyance

--- src/middlewares/headers.ts
+++ src/middlewares/headers.ts
@@ -13,8 +13,8 @@ export default class extends Middleware {
 		response.setHeader('Access-Control-Allow-Origin', '*');
 		response.setHeader('Access-Control-Allow-Methods', 'DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT');
 		response.setHeader('Access-Control-Allow-Headers', 'Authorization, User-Agent, Content-Type');
+		response.setHeader('Content-Type', 'application/json');
 		if (request.method === 'OPTIONS') response.end('{"success":true}');
-		else response.setHeader('Content-Type', 'application/json');
 	}
 
 }
